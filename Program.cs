1f833184f968e5a78f924723327da38daf0fee9d
jhwil90 <31808102+jhwil90@users.noreply.github.com>
2017-09-29 08:24:57 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus Casino! Roll the dice? (Y/N)");
+            int choice = Console.ReadLine().ToUpper();
+            
+            if (UserChoice = "Y");
+
+            else
+            {
+                Keep = false;
+            }
+            int dieOneRollOne;
+            string UserChoice = "Y";
+            UserChoice = Console.ReadLine();
+            if (UserChoice == "Y" || UserChoice == "Y")
+            {
+                Console.Write("");
+                Console.Write("Enter the number of sides for a pair of dice:");
+                dieOneRollOne = int.Parse(Console.ReadLine());
+                       Console.WriteLine("");
+                Console.Write("Roll 1:");
+
+            }
+        }
+    }
+}
+}
